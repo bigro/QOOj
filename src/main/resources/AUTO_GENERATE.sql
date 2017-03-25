7b4972be97a5c8f39149f4681e27643955078811
jnishida <nishida@allora-inc.jp>
2017-03-25 18:01:49 +0900

    それっぽいプロダクトコード書いた

--- src/main/resources/AUTO_GENERATE.sql
+++ src/main/resources/AUTO_GENERATE.sql
@@ -7,7 +7,7 @@ create table test1 (
 );
 
 insert into test1 values(1, 'AAA', '2017-04-04', now(), '10:10:10');
-insert into test1 values(4, 'AAA', '2017-04-14', now(), '10:10:13');
-insert into test1 values(5, 'AAA', '2017-04-03', now(), '10:13:10');
-insert into test1 values(2, 'AAA', '2017-04-11', now(), '13:10:10');
-insert into test1 values(3, 'AAA', '2017-03-04', now(), '10:13:13');
+insert into test1 values(4, 'SSA', '2017-04-14', now(), '10:10:13');
+insert into test1 values(5, 'XXA', '2017-04-03', now(), '10:13:10');
+insert into test1 values(2, 'CCA', '2017-04-11', now(), '13:10:10');
+insert into test1 values(3, 'BBA', '2017-03-04', now(), '10:13:13');
